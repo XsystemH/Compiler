e4f42ceebaf1be158287f24115d0361b308a4ee4
XsystemH <1319971303@qq.com>
2024-10-30 02:59:52 +0800

    done: DA

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        String filename = "testcases/codegen/t14.mx";
+//        String filename = "testcases/codegen/e5.mx";
 //        InputStream input = new FileInputStream(filename);
 //        OutputStream IROut = new FileOutputStream("output.ll");
 //        OutputStream output = new FileOutputStream("output.s");
@@ -64,12 +64,10 @@ public class Main {
             for (Instr instr : irBuilder.program.instrs) {
                 if (instr instanceof funcDef func) {
                     func.cfg.rmPhi();
-//                    func.cfg.DCE();
                     func.cfg.linear_scan();
                 }
                 if (instr instanceof mainFn main) {
                     main.init.cfg.rmPhi();
-//                    main.init.cfg.DCE();
                     main.init.cfg.linear_scan();
                 }
             }
